70408dad73973ce7e7301b0a2ae068fd24df5ea8
timx0645 <31208263+timx0645@users.noreply.github.com>
2019-04-25 13:02:47 +0200

    Build

---
+++ build/precache-manifest.b3092e10758e74fde3c2fca308bfb2e9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c16e2e55c234ede179c1",
+    "url": "/static/js/main.98d21ccb.chunk.js"
+  },
+  {
+    "revision": "3f1523dc7a92488c7952",
+    "url": "/static/js/2.e0996640.chunk.js"
+  },
+  {
+    "revision": "c16e2e55c234ede179c1",
+    "url": "/static/css/main.cc0550ec.chunk.css"
+  },
+  {
+    "revision": "f1166243190b8b933e6c8eaf1959be4e",
+    "url": "/index.html"
+  }
+];
