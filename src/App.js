cf1e52fb214180ef2b6bbb4e027cadb95ead7bae
timx0645 <31208263+timx0645@users.noreply.github.com>
2019-05-13 11:27:20 +0200

    hey

--- src/App.js
+++ src/App.js
@@ -7,32 +7,57 @@ import Name from "./name";
 import Game from "./NewGame";
 import history from './history';
 import Rankscore from './Rankscore';
-import Rank from './score';
 
 
 class App extends Component {
+  API_URL = "https://spermbank.herokuapp.com/api/";
   constructor() {
     super();
     this.state = { 
       Allscores: [],
       score: 4,
       sted: '',
-      name: ''
+      name: '',
+      online: false
     };
     this.LocationSubmit = this.LocationSubmit.bind(this);
     this.NameSubmit = this.NameSubmit.bind(this);
     this.Addscore = this.Addscore.bind(this);
     this.renderdata = this.renderdata.bind(this);
+    this.getData = this.getData.bind(this);
   }
 
   componentDidMount() {
-    db.table('Score')
+    this.renderdata();
+    this.interval = setInterval(() => this.renderdata(), 5000);
+    this.getData();
+  }
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  getData() {
+    if(navigator.onLine) {
+      fetch(`${this.API_URL}all`)
+          .then(response => response.json())
+          .then(data => {
+              this.setState({
+                Allscores: data
+              });
+              console.log(data)
+          })
+          .catch(error => {
+              console.error("Error when fetching: ", error);
+          })
+    } else {
+      db.table('Score')
       .toArray()
       .then((Score) => {
         this.setState({ 
           Allscores: Score
         });
       });
+    }
   }
 
   LocationSubmit(event) {
@@ -65,25 +90,44 @@ class App extends Component {
   }
 
   renderdata() {
-    if(!db.table('Score') === []) {
+    if(navigator.onLine) {
+      this.setState({
+        online: true
+      })
+    } else {
+      this.setState({
+        online: false
+      })
+    }
+
+    if (navigator.onLine && !db.table('Score') === []) {
       db.table('Score').foreach( e => {
         let navn = e.name;
         let lokation = e.location;
         let nummer = e.score;
-        Rank.create({ 
-          name: navn, 
-          location: lokation, 
-          score: nummer})
-          .catch(err => console.log(err.errors))
+        fetch(`${this.API_URL}/add`, {
+          method: 'POST',
+          headers: {
+              'Accept': 'application/json',
+              'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            Name: navn,
+            Location: lokation,
+            Score: nummer
+          })        
+          })
+          .catch(err => console.log(err));
       });
       db.table('Score').clear();
+      this.getData();
     }
   }
 
-  render() {
+  render() { 
     return (
       <Router history={history}>
-      <div className="container">
+      <div className={this.state.online ? "container border_green" : "container border_red"}>
           <Switch>
               <Route exact path={'/'}
                      render={(props) =>
